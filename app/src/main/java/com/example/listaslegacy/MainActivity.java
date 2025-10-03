8aabfd8c708a81b1acf833d24a10eeecb8d9b0a1
mateussperess <mateuspereslopesl@outlook.com>
2025-10-02 22:00:22 -0300

    mostrando lista como text view

--- app/src/main/java/com/example/listaslegacy/MainActivity.java
+++ app/src/main/java/com/example/listaslegacy/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.listaslegacy;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String animais[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        animais = new String[] {"Cachorro", "Gato", "Coelho", "Peixe", "Canguru"};
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, animais);
+//        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.item_da_lista, R.id.txtAnimais, animais);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.item_da_lista, R.id.txtAnimais, animais);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, animais[position], Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
